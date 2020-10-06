57421cbdd20e8b2bebe47b3700f6aecad25a55cc
Dev <info@yaddress.net>
2020-10-06 12:27:14 -0700

    Ver 1.1

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -8,8 +8,7 @@ namespace TestApp
         {
             YAddress.WebApiClient client = new YAddress.WebApiClient();
             YAddress.WebApiClient.Address adr = 
-                client.ProcessAddress("506 Fourth Avenue # 1", "Asbury Prk, NJ",
-                    "3DE15352-4B97-4E34-AA7C-570105C1C68B");
+                client.ProcessAddress("506 Fourth Avenue # 1", "Asbury Prk, NJ", null);
         }
     }
 }
